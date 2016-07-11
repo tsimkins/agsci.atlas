9cdac8223bdaf064a4d84483a42a4f0f08138df6
TIMOTHY RIDGE SIMKINS <trs22@psu.edu>
2016-07-11 13:56:02 -0400

    Added API logic for webinar object.

---
+++ agsci/atlas/api/webinar.py
@@ -0,0 +1,43 @@
+from agsci.api import BaseView
+from agsci.api.api.plone.file import FileView
+
+class WebinarView(BaseView):
+
+    def getData(self):
+        data = super(WebinarView, self).getData()
+
+        data['parent_id'] = self.context.getParentId()
+
+        # Get the webinar recording object, and attach its field as an item
+        pages = self.context.getPages()
+
+        if pages:
+            webinar_recording = pages[0]
+            link = getattr(webinar_recording, 'link', None)
+
+            if link:
+                data['webinar_recorded_url'] = link
+
+                # Now, attach the handouts and presentations
+                files = webinar_recording.getPages()
+
+                if files:
+                    data['webinar_recorded_files'] = []
+
+                    for i in files:
+                        file_api_view = i.restrictedTraverse('@@api')
+                        data['webinar_recorded_files'].append(file_api_view.getData())
+
+        return data
+
+class WebinarFileView(FileView):
+
+    def getData(self):
+        data = super(WebinarFileView, self).getData()
+
+        # Remove fields that are acquired from parent webinar
+        for i in ['event_start_date', 'event_end_date', 'description']:
+            if data.has_key(i):
+                del data[i]
+
+        return data
