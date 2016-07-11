5c0fb4c8c287e232db70797a04b8a0c6f6a7cb71
TIMOTHY RIDGE SIMKINS <trs22@psu.edu>
2016-07-11 13:53:59 -0400

    Made events a subclass of the Atlas container.

--- agsci/atlas/content/event/__init__.py
+++ agsci/atlas/content/event/__init__.py
@@ -6,11 +6,11 @@ from plone.supermodel import model
 from zope import schema
 from zope.component import adapter
 from zope.interface import provider, implementer
-from plone.dexterity.content import Container
 from plone.app.contenttypes.interfaces import IEvent as _IEvent
 from plone.app.textfield import RichText
 from zope.schema.vocabulary import SimpleTerm
 from group import IEventGroup
+from .. import Container
 from ..behaviors import IAtlasLocation, IAtlasForSaleProduct
 
 # Event
