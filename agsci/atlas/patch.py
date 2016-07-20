76a716025bb81f59e835d4c50824715a4bafbd9f
TIMOTHY RIDGE SIMKINS <trs22@psu.edu>
2016-07-20 15:17:50 -0400

    Added a monkey patch to bring the Dexterity diff behavior up to Products.CMFDiffTool v3.0.4. This added all of the fields, rather than just the default form.

--- agsci/atlas/patch.py
+++ agsci/atlas/patch.py
@@ -1,5 +1,7 @@
 from zope.event import notify
 from zope.lifecycleevent import ObjectModifiedEvent
+from plone.dexterity.browser.edit import DefaultEditForm
+from zope.globalrequest import getRequest
 
 
 # This patch adds an 'update' method to the EventAccessor class.  The 'edit`
@@ -16,3 +18,18 @@ def event_accessor_update(self, **kwargs):
             setattr(self.context, key, value)
 
     notify(ObjectModifiedEvent(self.context))
+
+# From Products.CMFDiffTool.dexteritydiff.DexterityCompoundDiff._compute_fields_order
+# Patching in from v3.0.4 (2016-02-27)
+
+def patched_compute_fields_order(self, obj):
+    form = DefaultEditForm(obj, getRequest())
+    form.portal_type = obj.portal_type
+    form.updateFields()
+    all_fields = list()
+    all_fields += [(form.fields[name].field, name) for name in form.fields]
+    if form.groups:
+        for group in form.groups:
+            all_fields += [(group.fields[name].field, name) for name in group.fields]
+
+    return all_fields
