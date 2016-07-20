a777819e5ff0eb74bd708c5ed9fe2a17682b5573
TIMOTHY RIDGE SIMKINS <trs22@psu.edu>
2016-07-20 10:34:16 -0400

    Made location fields on Cvent event read-only.

--- agsci/atlas/content/event/cvent/__init__.py
+++ agsci/atlas/content/event/cvent/__init__.py
@@ -1,9 +1,28 @@
 from .. import Event, ILocationEvent
 from agsci.atlas import AtlasMessageFactory as _
+from agsci.atlas.content.behaviors import IAtlasLocation
 from plone.supermodel import model
 from zope import schema
+from plone.autoform import directives as form
+
+# Define read permission
+write_permission = 'agsci.atlas.super'
 
 class ICventEvent(ILocationEvent):
+
+    def getReadOnlyFieldConfig(v):
+    
+        # Initialize read-only fields with location names
+        read_only_fields = list(IAtlasLocation.names())
+        
+        # Append Cvent fields to read_only_fields
+        read_only_fields.extend(['cvent_id', 'cvent_url'])
+        
+        # Transform list into kw dictionary and return
+        return dict([(x, v) for x in read_only_fields])
+        
+    # Set write permissions for form.
+    form.write_permission(**getReadOnlyFieldConfig(write_permission))
 
     model.fieldset(
             'internal',
