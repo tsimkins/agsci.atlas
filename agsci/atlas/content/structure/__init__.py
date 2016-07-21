ba612dab0b2e2a32fc082024feac81ad046de147
TIMOTHY RIDGE SIMKINS <trs22@psu.edu>
2016-07-21 11:50:30 -0400

    Customized the `allowedContentTypes` method for Category Level 2, since we can have Products *or*  a Category Level 3 inside, but not both.

--- agsci/atlas/content/structure/__init__.py
+++ agsci/atlas/content/structure/__init__.py
@@ -39,7 +39,33 @@ class CategoryLevel1(AtlasStructure):
 @adapter(ICategoryLevel2)
 class CategoryLevel2(AtlasStructure):
 
-    pass
+    # Customize this method for Category Level 2, since we can have Products *or* 
+    # a Category Level 3 inside, but not both.
+    def allowedContentTypes(self):
+
+        # Get existing allowed types
+        allowed_content_types = super(CategoryLevel2, self).allowedContentTypes()    
+        
+        # Remove some content types if we have a Category Level 3 inside us        
+        if self.listFolderContents({'Type' : 'CategoryLevel3'}):
+
+            # Define permitted type ids
+            restricted_to_types = [
+                                    'atlas_category_level_3', 
+                                    'atlas_events_container', 
+                                    'atlas_news_container'
+                                ]
+
+            # This is the list we'll be returning
+            final_types = []
+            
+            for i in allowed_content_types:
+                if i.getId() in restricted_to_types:
+                    final_types.append(i)
+
+            return final_types
+
+        return allowed_content_types
 
 @adapter(ICategoryLevel3)
 class CategoryLevel3(AtlasStructure):
