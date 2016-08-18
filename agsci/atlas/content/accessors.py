dfeb24b1dd1f7a2771f54adcfcf081942dfe5360
TIMOTHY RIDGE SIMKINS <trs22@psu.edu>
2016-08-18 16:10:14 -0400

    Made AtlasEventAccessorFactory dependent on .Type() passed in as a parameter.

--- agsci/atlas/content/accessors.py
+++ agsci/atlas/content/accessors.py
@@ -1,11 +1,14 @@
 from plone.app.event.dx.behaviors import EventAccessor
 
 # Atlas Cvent Event Accessor subclass
-
 class AtlasCventEventAccessor(EventAccessor):
     event_type = 'atlas_cvent_event'
 
 # Factory that takes in product type and returns the correct accessor subclass
-
+# Note: The value for `product_type` is .Type(), not .portal_type
 def AtlasEventAccessorFactory(product_type):
-    return AtlasCventEventAccessor
+
+    if product_type in ('Cvent Event'):
+        return AtlasCventEventAccessor
+
+    return EventAccessor
