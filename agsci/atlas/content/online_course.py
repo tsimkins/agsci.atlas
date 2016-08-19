8dfaf5d11bdbd8d07d5a6e0c2649d16553118929
TIMOTHY RIDGE SIMKINS <trs22@psu.edu>
2016-08-19 12:56:21 -0400

    Tweaks to online course schema, including moving edX Id field to the 'Internal' tab, and making video fields not required.

--- agsci/atlas/content/online_course.py
+++ agsci/atlas/content/online_course.py
@@ -1,18 +1,45 @@
 from agsci.atlas import AtlasMessageFactory as _
 from plone.autoform.interfaces import IFormFieldProvider
+from plone.autoform import directives as form
 from zope import schema
 from zope.component import adapter
 from zope.interface import provider
 from . import Container, IAtlasProduct
 from .behaviors import IVideoBase
+from plone.supermodel import model
+
+import copy
 
 @provider(IFormFieldProvider)
 class IOnlineCourse(IVideoBase, IAtlasProduct):
 
+    # Hide the video 'channel' field
+    form.omitted('channel')
+    
+    # Internal
+    model.fieldset(
+            'internal',
+            label=_(u'Internal'),
+            fields=['edx_id',],
+        )
+
     # Should this just be the SKU?
     edx_id = schema.TextLine(
         title=_(u"edX Id"),
+        required=False,
     )
+    
+    # Duplicates the following fields from the IVideoBase parent schema, makes 
+    # a copy, and makes the copy not required.
+    link = copy.copy(IVideoBase.get('link'))
+    link.required = False
+    
+    provider = copy.copy(IVideoBase.get('provider'))
+    provider.required = False
+        
+    aspect_ratio = copy.copy(IVideoBase.get('aspect_ratio'))
+    aspect_ratio.required = False
+
 
 @adapter(IOnlineCourse)
 class OnlineCourse(Container):
