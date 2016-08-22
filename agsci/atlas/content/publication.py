0a37a3213c8e6ce64a6aefcd8f833540f878fb3a
TIMOTHY RIDGE SIMKINS <trs22@psu.edu>
2016-08-22 15:29:09 -0400

    * Added dynamic calculation of pages in PDF publication to API view. Can still override by manually setting field.

--- agsci/atlas/content/publication.py
+++ agsci/atlas/content/publication.py
@@ -4,7 +4,14 @@ from plone.namedfile.field import NamedBlobFile
 from zope import schema
 from zope.component import adapter
 from zope.interface import provider
+from StringIO import StringIO
 from . import Container, IAtlasProduct
+
+try:
+    from pyPdf import PdfFileReader
+except ImportError:
+    def PdfFileReader(*args, **kwargs):
+        return None
 
 @provider(IFormFieldProvider)
 class IPublication(IAtlasProduct):
@@ -25,11 +32,31 @@ class IPublication(IAtlasProduct):
 
     pages_count = schema.Int(
         title=_(u"Page Count"),
-        description=_(u""),
+        description=_(u"Manually set page count."),
         required=False,
     )
 
 @adapter(IPublication)
 class Publication(Container):
 
-    pass
+    # If the page count is not manually set PDF is attached, automagically grab the page count for the API
+    def getPageCount(self):
+    
+        # Get the hardcoded page count, and return if it's there
+        page_count = getattr(self, 'pages_count', None)
+
+        if page_count:
+            return page_count
+
+        # Otherwise, grab the page count from the attached PDF.
+        elif self.pdf:
+        
+            if self.pdf.data and self.pdf.contentType == 'application/pdf':
+                try:
+                    pdf_data = StringIO(self.pdf.data)
+                    pdf = PdfFileReader(pdf_data)
+                    return pdf.getNumPages()
+                except:
+                    pass
+
+        return
