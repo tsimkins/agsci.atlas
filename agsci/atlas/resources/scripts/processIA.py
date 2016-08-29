10e3b926d6b3a93ce5a5a05d70d9d5f167b34d8d
TIMOTHY RIDGE SIMKINS <trs22@psu.edu>
2016-08-29 14:25:09 -0400

    Adding scripts to produce JSON files from text files.

---
+++ agsci/atlas/resources/scripts/processIA.py
@@ -0,0 +1,55 @@
+import json
+
+data = {}
+
+# Set up the hierarchy of category types
+content_types = ['atlas_category_level_1', 'atlas_category_level_2', 'atlas_category_level_3']
+
+# Pull that into a containers dict
+containers = dict([(x,None) for x in content_types])
+
+for i in open("ia.txt", "r").readlines():
+
+    # Initialize filter_sets
+    filter_sets = None
+
+    # Ignore blank lines
+    if not i.strip():
+        continue
+
+    # Ignore comments lines
+    if i.strip().startswith('#'):
+        continue
+
+    # Get the level (1,2,3) and content_type based on the number of tabs
+    level = i.count('\t')
+    content_type = content_types[level]
+
+    # Get the human readable name, and make it into a short name
+    name = i.strip()
+    
+    # If the name contains filter_sets, split it out into the names and filters
+    # Split the filters on ;
+    if '|' in name:
+        (name, filter_sets) = name.split('|')
+        filter_sets = map(lambda x: x.strip(), filter_sets.split(';'))
+
+
+    if level == 0:
+        container_object = data
+    else:
+        container_object = containers.get(content_types[level-1], None)
+
+    if container_object.has_key(name):
+        item = container_object[name]
+
+    else:
+        if filter_sets:
+            item = list(filter_sets)
+        else:
+            item = {}
+        container_object[name] = item
+    
+    containers[content_type] = item
+
+print json.dumps(data, indent=4, sort_keys=True)
---
+++ agsci/atlas/resources/scripts/processStructure.py
@@ -0,0 +1,49 @@
+import json
+
+data = {}
+
+# Set up the hierarchy of category types
+content_types = ['atlas_state_extension_team', 'atlas_program_team', 'curricula']
+
+# Pull that into a containers dict
+containers = dict([(x,None) for x in content_types])
+
+for i in sorted(open("extension_structure.txt", "r").readlines()):
+
+    # Ignore blank lines
+    if not i.strip():
+        continue
+
+    # Ignore comments lines
+    if i.strip().startswith('#'):
+        continue
+
+    values = i.strip().split('\t')
+
+    for level in range(0, len(values)):
+
+        content_type = content_types[level]
+    
+        # Get the human readable name, and make it into a short name
+        name = values[level].strip()
+        
+        if level == 0:
+            container_object = data
+        else:
+            container_object = containers.get(content_types[level-1], None)
+
+
+        if container_object.has_key(name):
+            item = container_object[name]
+        else:
+            item = {}
+            container_object[name] = item
+        
+        containers[content_type] = item
+
+for (k0,v0) in data.iteritems():
+    for (k1,v1) in v0.iteritems():
+        curricula = v1.keys()
+        v0[k1] = curricula
+
+print json.dumps(data, indent=4, sort_keys=True)
