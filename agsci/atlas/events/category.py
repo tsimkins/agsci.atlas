ee029a4bac5ecaed8c9c7d901ec599bc7f2ce7c3
TIMOTHY RIDGE SIMKINS <trs22@psu.edu>
2016-10-03 12:48:52 -0400

    * Using 'IObjectAddedEvent' event because we have the Acquisition-wrapped context.  * Using AtlasMetadataCalculator to get the CategoryLevel1:CategoryLevel2 name to use as the group name.

--- agsci/atlas/events/category.py
+++ agsci/atlas/events/category.py
@@ -1,3 +1,4 @@
+from agsci.atlas.content.vocabulary.calculator import AtlasMetadataCalculator
 from agsci.atlas.utilities import ploneify
 from Products.CMFCore.utils import getToolByName
 from zope.component.hooks import getSite
@@ -6,25 +7,39 @@ from zope.component.hooks import getSite
 # assigns Add/Edit
 def onCategoryCreate(context, event):
 
-    # Calculate the group id and title
-    title = context.Title()
-    group_id = '%s-editors' % ploneify(title)
-    group_title = '%s Editors' % title
+    try:
 
-    # Get the group tool
-    site = getSite()
-    grouptool = getToolByName(site, 'portal_groups')
+        mc = AtlasMetadataCalculator(context.Type())
 
-    # Add the new group
-    grouptool.addGroup(group_id)
+    except ValueError:
 
-    # Get the group
-    group = grouptool.getGroupById(group_id)
+        # Not a valid content type
+        return
 
-    # Set the group title
-    group.title = group_title
-    group.setGroupProperties({'title' : group_title})
+    else:
 
-    # Set roles on context
-    context.manage_setLocalRoles(group_id, ['Contributor', 'Editor', 'Reader'])
-    context.reindexObjectSecurity()
+        # Calculate the group id and title
+        title = mc.getMetadataForObject(context)
+        group_id = '%s-editors' % ploneify(title)
+        group_title = '%s Editors' % title
+
+        # Get the group tool
+        site = getSite()
+        grouptool = getToolByName(site, 'portal_groups')
+
+        # If our group does not exist, create it and set roles.
+        if not grouptool.getGroupById(group_id):
+
+            # Add the new group
+            grouptool.addGroup(group_id)
+
+            # Get the group
+            group = grouptool.getGroupById(group_id)
+
+            # Set the group title
+            group.title = group_title
+            group.setGroupProperties({'title' : group_title})
+
+            # Set roles on context
+            context.manage_setLocalRoles(group_id, ['Contributor', 'Editor', 'Reader'])
+            context.reindexObjectSecurity()
