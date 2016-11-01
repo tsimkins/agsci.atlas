075dbdefa8783ab443f15ecab9b62514890f6c7e
TIMOTHY RIDGE SIMKINS <trs22@psu.edu>
2016-11-01 16:07:10 -0400

    Updated user view of workshop and webinar types.

--- agsci/atlas/browser/views/product.py
+++ agsci/atlas/browser/views/product.py
@@ -1,10 +1,16 @@
 from plone.app.event.browser.event_view import EventView as _EventView
 
 from agsci.atlas.interfaces import IArticleMarker, INewsItemMarker, \
-                                   ISlideshowMarker
+                                   ISlideshowMarker, IVideoMarker, \
+                                   IWebinarMarker, IWorkshopMarker
+
 from agsci.atlas.utilities import increaseHeadingLevel
 
+import pytz
+
 from .base import BaseView
+
+from datetime import datetime
 
 class ProductView(BaseView):
 
@@ -27,7 +33,6 @@ class ArticleView(ProductView):
 
 
 class ArticleContentView(ProductView):
-
     pass
 
 
@@ -46,10 +51,10 @@ class SlideshowView(ArticleContentView):
 class VideoView(ArticleContentView):
 
     def getVideoId(self):
-        return self.context.getVideoId()
+        return IVideoMarker(self.context).getVideoId()
 
     def getVideoProvider(self):
-        return self.context.getVideoProvider()
+        return IVideoMarker(self.context).getVideoProvider()
 
 
 class WebinarRecordingView(ProductView):
@@ -67,36 +72,48 @@ class WebinarRecordingView(ProductView):
         return getattr(self.context, 'link', None)
 
 
-class EventView(_EventView):
+class EventView(_EventView, ProductView):
 
-    pass
+    def fmt(self, datestamp):
 
+        if isinstance(datestamp, datetime):
+            tz = pytz.timezone(self.context.timezone)
+            localized_datetime = datestamp.astimezone(tz)
+            return localized_datetime.strftime('%B %d, %Y %I:%M%p').replace(' 0', ' ')
+
+        return 'N/A'
+
+    def start(self):
+        return self.fmt(self.context.start)
+
+    def end(self):
+        return self.fmt(self.context.end)
+
+
+class WorkshopView(EventView):
+
+    def pages(self):
+        return IWorkshopMarker(self.context).getPageBrains()
+
+class WebinarView(EventView):
+
+    def pages(self):
+        return IWebinarMarker(self.context).getPageBrains()
 
 class PublicationView(ProductView):
-
     pass
-
 
 class ToolApplicationView(ProductView):
-
     pass
-
 
 class CurriculumView(ProductView):
-
     pass
-
 
 class WorkshopGroupView(ProductView):
-
     pass
-
 
 class WebinarGroupView(ProductView):
-
     pass
 
-
 class OnlineCourseView(ProductView):
-
     pass
