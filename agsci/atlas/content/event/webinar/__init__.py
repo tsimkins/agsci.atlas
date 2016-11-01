57d42fa93c0458e89f1a8f9e2a28b2816acef0e7
TIMOTHY RIDGE SIMKINS <trs22@psu.edu>
2016-11-01 13:42:42 -0400

    Restricted addable content types for webinar if a recording exists inside it.

--- agsci/atlas/content/event/webinar/__init__.py
+++ agsci/atlas/content/event/webinar/__init__.py
@@ -2,10 +2,19 @@ from agsci.atlas import AtlasMessageFactory as _
 from plone.supermodel import model
 from zope import schema
 from .. import Event, IWebinarLocationEvent, IRegistrationEvent
+from agsci.atlas.interfaces import IWebinarMarker
 
 class IWebinar(IRegistrationEvent, IWebinarLocationEvent):
     pass
 
 class Webinar(Event):
 
-    pass
+    # Only allow one webinar recording inside a webinar.
+    def allowedContentTypes(self):
+
+        # If we find a recording, return an empty list
+        if IWebinarMarker(self).getPages():
+            return []
+
+        # Otherwise, return the default.
+        return super(Webinar, self).allowedContentTypes()
