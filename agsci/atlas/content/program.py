059aedc0a4c7b582eb3f9f03d1bbeefc0258dffa
TIMOTHY RIDGE SIMKINS <trs22@psu.edu>
2017-01-18 13:29:28 -0500

    Added 'Program' and 'Hyperlink' products.

---
+++ agsci/atlas/content/program.py
@@ -0,0 +1,32 @@
+from agsci.atlas import AtlasMessageFactory as _
+from plone.autoform.interfaces import IFormFieldProvider
+from plone.namedfile.field import NamedBlobFile
+from zope import schema
+from zope.interface import provider
+from . import Container, IAtlasProduct
+
+@provider(IFormFieldProvider)
+class IProgram(IAtlasProduct):
+
+    __doc__ = 'Program Data'
+
+    external_url = schema.TextLine(
+        title=_(u"External URL"),
+        description=_(u""),
+        required=True,
+    )
+
+
+class Program(Container):
+
+    pass
+
+@provider(IFormFieldProvider)
+class IProgramLink(IProgram):
+
+    pass
+
+
+class ProgramLink(Program):
+
+    pass
