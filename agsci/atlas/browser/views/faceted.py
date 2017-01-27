63ef1b28da93fb11a3f76920139b175af0111001
TIMOTHY RIDGE SIMKINS <trs22@psu.edu>
2017-01-27 09:25:49 -0500

    Unbatching.

--- agsci/atlas/browser/views/faceted.py
+++ agsci/atlas/browser/views/faceted.py
@@ -1,5 +1,8 @@
 from eea.facetednavigation.browser.app.query import FacetedQueryHandler
 from . import AtlasStructureView
+from eea.facetednavigation.caching import ramcache
+from eea.facetednavigation.caching import cacheKeyFacetedNavigation
+
 class AtlasStructureFacetedQueryHandler(AtlasStructureView, FacetedQueryHandler):
     
     def criteria(self, sort=False, **kwargs):
@@ -7,3 +10,10 @@ class AtlasStructureFacetedQueryHandler(AtlasStructureView, FacetedQueryHandler)
         faceted_query = super( AtlasStructureFacetedQueryHandler, self).criteria(sort, **kwargs)
         query.update(faceted_query)
         return query
+
+    @ramcache(cacheKeyFacetedNavigation, dependencies=['eea.facetednavigation'])
+    def __call__(self, *args, **kwargs):
+        kwargs['batch'] = False
+        self.brains = self.query(**kwargs)
+        html = self.index()
+        return html
