8712128769ca04db1075afdc2f4c37b6f7ad5590
TIMOTHY RIDGE SIMKINS <trs22@psu.edu>
2017-02-14 12:23:31 -0500

    Added validation for `product_detail` against the schema.

--- agsci/atlas/browser/views/sync/cvent.py
+++ agsci/atlas/browser/views/sync/cvent.py
@@ -1,10 +1,18 @@
 from agsci.atlas.content.accessors import AtlasEventAccessorFactory
+from dateutil import parser as date_parser
 from plone.app.textfield.value import RichTextValue
 from plone.event.interfaces import IEventAccessor
 from zope.event import notify
 from zope.lifecycleevent import ObjectModifiedEvent
+from zope.schema.interfaces import WrongType
+from zope import schema
 
 from . import SyncContentView
+
+from agsci.atlas.utilities import default_timezone
+from agsci.atlas.content.event.cvent import ICventProductDetailRowSchema
+
+import pytz
 
 # View that accepts a POST of JSON data, and creates an event in Plone that
 # references the Cvent event id.
@@ -77,27 +85,58 @@ class SyncCventView(SyncContentView):
                           outputMimeType='text/x-html-safe')
 
         # Update the agenda
-        if v.data.event_agenda:
+        if v.data.product_detail:
 
             # Validate input data
-            if not isinstance(v.data.event_agenda, list):
-                raise TypeError('event_agenda is not an array')
-            else:
-                for i in v.data.event_agenda:
-                    if not isinstance(i, dict):
-                        raise TypeError('event_agenda item is not an associative array')
-                    else:
-                        input_keys = set(i.keys())
-                        expected_keys = set(['description', 'time', 'title'])
+            if not isinstance(v.data.product_detail, list):
+                raise TypeError('product_detail is not an array')
 
-                        missing_keys = list(expected_keys - input_keys)
+            else:
+                for i in v.data.product_detail:
+
+                    if not isinstance(i, dict):
+                        raise TypeError('product_detail item is not an associative array')
+                    else:
+                        # Validate incoming data structure against
+                        # ICventProductDetailRowSchema
+
+                        # Check for extra keys
+                        input_keys = set(i.keys())
+                        expected_keys = set(ICventProductDetailRowSchema.names())
+
                         extra_keys = list(input_keys - expected_keys)
 
-                        if missing_keys:
-                            raise ValueError('event_agenda missing keys %s' % repr(missing_keys))
-
                         if extra_keys:
-                            raise ValueError('event_agenda has extra keys %s' % repr(extra_keys))
+                            raise ValueError('product_detail has extra keys %s' % repr(extra_keys))
 
-            # Set event agenda to event_agenda
-            context.agenda = v.data.event_agenda
+                        # Check for valid data types
+                        for (field_name, field) in ICventProductDetailRowSchema.namesAndDescriptions():
+                            if i.has_key(field_name):
+
+                                # Pre-process datetimes
+                                if isinstance(field, schema.Datetime):
+                                    try:
+                                        field_value = date_parser.parse(i[field_name])
+                                    except:
+                                        # Let the validation catch it. Not a datetime
+                                        pass
+                                    else:
+                                        # if it's a naive timezone, set it to Eastern
+                                        if not field_value.tzinfo:
+                                            i[field_name] =  pytz.timezone(default_timezone).localize(field_value)
+                                        else:
+                                            i[field_name] = field_value
+
+                                # Run the validation for the schema field against
+                                # the incoming data
+                                try:
+                                    field.validate(i[field_name])
+
+                                except WrongType, e:
+                                    raise ValueError("Wrong type for %s: expected %s, not %s" % (field_name, e.args[1].__name__, e.args[0].__class__.__name__))
+
+                                except:
+                                    raise ValueError("Error with %s" % field_name)
+
+            # Set event agenda to product_detail
+            context.product_detail = v.data.product_detail
