e21d30f6d7d66e74c8caceddd6bdf9d014786ce8
TIMOTHY RIDGE SIMKINS <trs22@psu.edu>
2017-03-15 08:58:12 -0400

    Added Magento catalog visibility constants (so I don't typo something)

--- agsci/atlas/constants.py
+++ agsci/atlas/constants.py
@@ -1,2 +1,7 @@
 ACTIVE_REVIEW_STATES = ['requires_initial_review', 'pending', 'published',
                         'expiring_soon', 'requires_feedback',  'private']
+
+# Catalog Visibility
+
+V_CS = 'Catalog, Search'
+V_NVI = 'Not Visible Individually'
