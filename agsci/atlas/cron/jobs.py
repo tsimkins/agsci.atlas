d0f9948b84d15dd1b7748cefb121e1c5de09d84c
TIMOTHY RIDGE SIMKINS <trs22@psu.edu>
2017-06-27 14:26:23 -0400

    Added a job to deactivate expired people.

--- agsci/atlas/cron/jobs.py
+++ agsci/atlas/cron/jobs.py
@@ -1,3 +1,5 @@
+from Products.CMFPlone.utils import safe_unicode
+
 from . import CronJob
 
 class ExpireExpiredProducts(CronJob):
@@ -5,4 +7,47 @@ class ExpireExpiredProducts(CronJob):
     title = "Expire published products that have an expiration date in the past."
 
     def run(self):
-        self.log("Expired N items")
+    
+        results = self.portal_catalog.searchResults({
+            'object_provides' : 'agsci.atlas.content.IAtlasProduct',
+            'expires' : {
+                'range' : 'max',
+                'query' : self.now,
+            },
+            'review_state' : 'published',
+        })
+        
+        msg = "Automatically expired based on expiration date."
+        
+        for r in results:
+            o = r.getObject()
+            
+            self.portal_workflow.doActionFor(o, 'expired', comment=msg)
+            o.reindexObject()
+            
+            self.log(u"Expired %s %s (%s)" % (r.Type, safe_unicode(r.Title), r.getURL()))
+
+class DeactivateExpiredPeople(CronJob):
+
+    title = "Deactivate active people that have an expiration date in the past."
+
+    def run(self):
+    
+        results = self.portal_catalog.searchResults({
+            'object_provides' : 'agsci.person.content.person.IPerson',
+            'expires' : {
+                'range' : 'max',
+                'query' : self.now,
+            },
+            'review_state' : 'published',
+        })
+
+        msg = "Automatically deactivated based on expiration date."
+        
+        for r in results:
+            o = r.getObject()
+            
+            self.portal_workflow.doActionFor(o, 'deactivate', comment=msg)
+            o.reindexObject()
+            
+            self.log(u"Deactivated %s %s (%s)" % (r.Type, safe_unicode(r.Title), r.getURL()))
