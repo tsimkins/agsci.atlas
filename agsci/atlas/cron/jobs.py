5101201e6ae04f68367ed5efea017a912e9008fd
TIMOTHY RIDGE SIMKINS <trs22@psu.edu>
2017-06-27 14:54:34 -0400

    Added job to set products that are expiring soon to "Expiring Soon"

--- agsci/atlas/cron/jobs.py
+++ agsci/atlas/cron/jobs.py
@@ -7,24 +7,24 @@ class ExpireExpiredProducts(CronJob):
     title = "Expire published products that have an expiration date in the past."
 
     def run(self):
-    
+
         results = self.portal_catalog.searchResults({
             'object_provides' : 'agsci.atlas.content.IAtlasProduct',
             'expires' : {
                 'range' : 'max',
                 'query' : self.now,
             },
-            'review_state' : 'published',
+            'review_state' : ['published', 'expiring_soon'],
         })
-        
+
         msg = "Automatically expired based on expiration date."
-        
+
         for r in results:
             o = r.getObject()
-            
+
             self.portal_workflow.doActionFor(o, 'expired', comment=msg)
             o.reindexObject()
-            
+
             self.log(u"Expired %s %s (%s)" % (r.Type, safe_unicode(r.Title), r.getURL()))
 
 class DeactivateExpiredPeople(CronJob):
@@ -32,7 +32,7 @@ class DeactivateExpiredPeople(CronJob):
     title = "Deactivate active people that have an expiration date in the past."
 
     def run(self):
-    
+
         results = self.portal_catalog.searchResults({
             'object_provides' : 'agsci.person.content.person.IPerson',
             'expires' : {
@@ -43,11 +43,39 @@ class DeactivateExpiredPeople(CronJob):
         })
 
         msg = "Automatically deactivated based on expiration date."
-        
+
         for r in results:
             o = r.getObject()
-            
+
             self.portal_workflow.doActionFor(o, 'deactivate', comment=msg)
             o.reindexObject()
-            
+
             self.log(u"Deactivated %s %s (%s)" % (r.Type, safe_unicode(r.Title), r.getURL()))
+
+
+class SetExpiringSoonProducts(CronJob):
+
+    future = 3*31 # 3 months
+
+    title = "Set published products that will be expiring soon to the 'Expiring Soon' state."
+
+    def run(self):
+
+        results = self.portal_catalog.searchResults({
+            'object_provides' : 'agsci.atlas.content.IAtlasProduct',
+            'expires' : {
+                'range' : 'max',
+                'query' : self.now + self.future,
+            },
+            'review_state' : ['published'],
+        })
+
+        msg = "Set to 'Expiring Soon' based on pending expiration date."
+
+        for r in results:
+            o = r.getObject()
+
+            self.portal_workflow.doActionFor(o, 'expiring_soon', comment=msg)
+            o.reindexObject()
+
+            self.log(u"Set %s '%s' (%s) to 'Expiring Soon'" % (r.Type, safe_unicode(r.Title), r.getURL()))
