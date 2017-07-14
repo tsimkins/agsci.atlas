f4cac0177200ab22d967c07cd7da77980f381464
TIMOTHY RIDGE SIMKINS <trs22@psu.edu>
2017-07-14 12:45:51 -0400

    Moved the image formats data into .constants so we can reference it other places.

--- agsci/atlas/constants.py
+++ agsci/atlas/constants.py
@@ -12,3 +12,11 @@ DEFAULT_TIMEZONE = 'US/Eastern'
 
 # Delimiter for categories/teams
 DELIMITER = '|'
+
+# Mimetype to PIL and filename extension
+IMAGE_FORMATS = {
+    'image/jpeg' : ['JPEG', 'jpg'],
+    'image/png' : ['PNG', 'png'],
+    'image/gif' : ['GIF', 'gif'],
+    'image/x-ms-bmp' : ['BMP', 'bmp'],
+}
