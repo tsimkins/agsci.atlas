7934bb1a33fbc3a6cb7a2c450a3aa9aa5bce9a00
TIMOTHY RIDGE SIMKINS <trs22@psu.edu>
2017-10-20 13:42:39 -0400

    Added a new "Workshop (External)" type, with some fields hidden, and an external_url field.

--- agsci/atlas/content/event/workshop/__init__.py
+++ agsci/atlas/content/event/workshop/__init__.py
@@ -1,5 +1,9 @@
 from plone.autoform.interfaces import IFormFieldProvider
+from plone.autoform import directives as form
 from zope.interface import provider
+from zope import schema
+
+from agsci.atlas import AtlasMessageFactory as _
 
 from .. import Event, ILocationEvent, IRegistrationEvent
 
@@ -9,3 +13,20 @@ class IWorkshop(IRegistrationEvent, ILocationEvent):
 
 class Workshop(Event):
     pass
+
+@provider(IFormFieldProvider)
+class IWorkshopExternal(IWorkshop):
+
+    # Hide registration fields
+
+    form.omitted(
+        'registration_status', 'capacity', 'registration_deadline',
+        'registrant_type', 'registration_help_name', 'registration_help_phone',
+        'registration_help_email', 'walkin', 'cancellation_deadline'
+    )
+
+    external_url = schema.TextLine(
+        title=_(u"External Workshop URL"),
+        description=_(u""),
+        required=True,
+    )
