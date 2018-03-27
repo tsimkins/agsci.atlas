b180f1ab7db2d24f2c76e9b0f9aaadf8044e2a19
TIMOTHY RIDGE SIMKINS <trs22@psu.edu>
2018-03-27 15:10:43 -0400

    Moved 'object_factory' into main agsci.atlas package so we can use it elsewhere.

--- agsci/atlas/content/check/__init__.py
+++ agsci/atlas/content/check/__init__.py
@@ -189,17 +189,7 @@ class ContentCheck(object):
 
     # Returns an object with the keyword arguments as properties
     def object_factory(self, **kwargs):
-
-        # https://stackoverflow.com/questions/1305532/convert-python-dict-to-object
-        class _(object):
-
-            def __init__(self, **kwargs):
-                self.__dict__.update(kwargs)
-
-                # Provide placeholder for empty text
-                if not getattr(self, 'text', ''):
-                    self.text = 'N/A'
-
+        from agsci.atlas import object_factory as _
         return _(**kwargs)
 
     @property
