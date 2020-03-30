342e0868772209cf82be8ad72d7f0a30dd335431
TIMOTHY RIDGE SIMKINS <trs22@psu.edu>
2020-03-30 17:41:05 -0400

    Added a method to create a webinar from a Cvent event with minimal effort.

--- agsci/atlas/content/accessors.py
+++ agsci/atlas/content/accessors.py
@@ -4,11 +4,18 @@ from plone.app.event.dx.behaviors import EventAccessor
 class AtlasCventEventAccessor(EventAccessor):
     event_type = 'atlas_cvent_event'
 
+# Atlas Webinar Accessor subclass
+class AtlasWebinarAccessor(EventAccessor):
+    event_type = 'atlas_webinar'
+
 # Factory that takes in product type and returns the correct accessor subclass
 # Note: The value for `product_type` is .Type(), not .portal_type
 def AtlasEventAccessorFactory(product_type):
 
     if product_type in ('Cvent Event'):
         return AtlasCventEventAccessor
+
+    elif product_type in ('Webinar'):
+        return AtlasWebinarAccessor
 
     return EventAccessor
