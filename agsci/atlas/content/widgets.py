713a3ef88ac71c421ceb21dac49976c8ae1c9191
TIMOTHY RIDGE SIMKINS <trs22@psu.edu>
2021-07-07 11:29:30 -0400

    This fix was superseded in a later version of plone.formwidget.datetime

--- agsci/atlas/content/widgets.py
+++ agsci/atlas/content/widgets.py
@@ -3,36 +3,47 @@ from datetime import date
 from plone.app.z3cform.widget import DatetimeFieldWidget as _DatetimeFieldWidget
 from plone.formwidget.datetime.z3cform.widget import DatetimeWidget as _DatetimeWidget
 
+import pkg_resources
 import z3c.form
 
-class DatetimeWidget(_DatetimeWidget):
+# This fix was superseded in a later version of plone.formwidget.datetime
+dist = pkg_resources.get_distribution("plone.formwidget.datetime")
 
-    @property
-    def years_range(self):
+if dist.version < '1.3.4':
 
-        # Stolen from collective.z3cform.datetimewidget.widget_date.DateWidget
+    class DatetimeWidget(_DatetimeWidget):
 
-        portal_properties = getToolByName(self.context, 'portal_properties', None)
+        @property
+        def years_range(self):
 
-        if portal_properties is not None:
-            p = portal_properties['site_properties']
-        else:
-            p = None
+            # Stolen from collective.z3cform.datetimewidget.widget_date.DateWidget
 
-        today = date.today()
+            portal_properties = getToolByName(self.context, 'portal_properties', None)
 
-        if self.field.min is not None:
-            start = self.field.min.year - today.year
-        else:
-            calendar_starting_year = getattr(p, 'calendar_starting_year', 2001)
-            start = calendar_starting_year - today.year
+            if portal_properties is not None:
+                p = portal_properties['site_properties']
+            else:
+                p = None
 
-        if self.field.max is not None:
-            end = self.field.max.year - today.year
-        else:
-            end = getattr(p, 'calendar_future_years_available', 5)
+            today = date.today()
 
-        return (start, end)
+            if self.field.min is not None:
+                start = self.field.min.year - today.year
+            else:
+                calendar_starting_year = getattr(p, 'calendar_starting_year', 2001)
+                start = calendar_starting_year - today.year
+
+            if self.field.max is not None:
+                end = self.field.max.year - today.year
+            else:
+                end = getattr(p, 'calendar_future_years_available', 5)
+
+            return (start, end)
+
+else:
+
+    class DatetimeWidget(_DatetimeWidget):
+        pass
 
 def DatetimeFieldWidget(field, request):
     return z3c.form.widget.FieldWidget(field, DatetimeWidget(request))
