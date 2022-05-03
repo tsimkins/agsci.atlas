58518cd91ff346cec3fb71c3d3034095b988c8cf
TIMOTHY RIDGE SIMKINS <trs22@psu.edu>
2022-05-03 15:26:55 -0400

    Added 'product_detail' to External Event

--- agsci/atlas/content/event/external/__init__.py
+++ agsci/atlas/content/event/external/__init__.py
@@ -1,3 +1,4 @@
+from collective.z3cform.datagridfield import DataGridFieldFactory, DictRow
 from plone.autoform.interfaces import IFormFieldProvider
 from plone.autoform import directives as form
 from zope.interface import provider
@@ -6,6 +7,7 @@ from zope import schema
 from agsci.atlas import AtlasMessageFactory as _
 
 from .. import Event, ILocationEvent, IRegistrationEvent
+from ..cvent import ICventProductDetailRowSchema
 
 @provider(IFormFieldProvider)
 class IExternalEvent(IRegistrationEvent, ILocationEvent):
@@ -13,8 +15,10 @@ class IExternalEvent(IRegistrationEvent, ILocationEvent):
     # Hide registration fields
 
     form.omitted(
-        'capacity', 'registrant_type', 'walkin',
+        'capacity', 'registrant_type', 'walkin', 'product_detail',
     )
+
+    form.widget(product_detail=DataGridFieldFactory)
 
     external_url = schema.TextLine(
         title=_(u"External Event URL"),
@@ -29,6 +33,12 @@ class IExternalEvent(IRegistrationEvent, ILocationEvent):
         required=True,
     )
 
+    # Product Detail - used for import/export only. Not editable in Plone
+    product_detail = schema.List(
+        title=u"Cvent Product Detail",
+        value_type=DictRow(title=u"Agenda Item", schema=ICventProductDetailRowSchema),
+        required=False
+    )
 
 class ExternalEvent(Event):
     pass
