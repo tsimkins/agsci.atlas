946088ac5135a8c5827395ddaa5f21bced34efc6
TIMOTHY RIDGE SIMKINS <trs22@psu.edu>
2023-07-14 12:35:45 -0400

    Added original (with minor tweaks) .create() and .edit() class methods back to EventAccessor class, since those were apparently removed for some reason after 1.2.x

--- agsci/atlas/content/accessors.py
+++ agsci/atlas/content/accessors.py
@@ -1,4 +1,34 @@
-from plone.app.event.dx.behaviors import EventAccessor
+from plone.app.event.dx.behaviors import EventAccessor as _EventAccessor
+from zope.event import notify
+from zope.lifecycleevent import ObjectModifiedEvent
+
+class EventAccessor(_EventAccessor):
+
+    # .create() and .edit() methods copied from v 1.2.x since they were (apparently?) remmoved
+
+    # Unified create method via Accessor
+    @classmethod
+    def create(cls, container, content_id, title, description=None,
+               start=None, end=None, timezone=None,
+               whole_day=None, open_end=None, **kwargs):
+        container.invokeFactory(cls.event_type,
+                                id=content_id,
+                                title=title,
+                                description=description,
+                                start=start,
+                                end=end,
+                                whole_day=whole_day,
+                                open_end=open_end,
+                                timezone=timezone)
+        content = container[content_id]
+        acc = cls(content)
+        acc.edit(**kwargs)
+        return acc
+
+    def edit(self, **kwargs):
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+        notify(ObjectModifiedEvent(self.context))
 
 # Atlas Cvent Event Accessor subclass
 class AtlasCventEventAccessor(EventAccessor):
