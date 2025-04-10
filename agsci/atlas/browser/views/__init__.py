fadabc7faba10d7dae8c75a5764ea32c50f63ec8
TIMOTHY RIDGE SIMKINS <trs22@psu.edu>
2025-04-10 12:27:50 -0400

    Added the campaign and comment fields for the QR code.

--- agsci/atlas/browser/views/__init__.py
+++ agsci/atlas/browser/views/__init__.py
@@ -1683,7 +1683,7 @@ class QRView(BaseView):
         self.request.set('disable_plone.rightcolumn',1)
         self.request.set('disable_plone.leftcolumn',1)
 
-    fields = "utm_source_medium,utm_source,utm_medium"
+    fields = "utm_source_medium,utm_source,utm_medium,utm_campaign,comment"
 
     @property
     def is_external(self):
