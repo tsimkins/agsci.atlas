b2e69b039980a24115d82dc29d9c14184049bdea
TIMOTHY RIDGE SIMKINS <trs22@psu.edu>
2026-09-23 10:59:01 -0400

    Fixing issue where the Under Review feature's custom form was preventing the related items widget from loading.

--- agsci/atlas/browser/views/edit.py
+++ agsci/atlas/browser/views/edit.py
@@ -1,6 +1,7 @@
 from Acquisition import aq_chain
-from plone.dexterity.browser.edit import DefaultEditForm
+from plone.dexterity.browser.edit import DefaultEditForm, DefaultEditView
 from plone.dexterity.interfaces import IDexterityEditForm
+from zope.interface import implementer
 from Products.CMFCore.utils import getToolByName
 from zope.security import checkPermission
 from plone.z3cform import layout
@@ -11,7 +12,6 @@ from agsci.atlas.content import IAtlasProduct
 
 import logging
 
-
 try:
     from plone.base.interfaces.siteroot import ISiteRoot
 except ImportError:
@@ -19,18 +19,39 @@ except ImportError:
 
 logger = logging.getLogger(__name__)
 
-
+@implementer(IDexterityEditForm)
 class ProductEditForm(DefaultEditForm):
+
     """
     Custom edit form that fires when the form loads (before save).
     Override the update() method which is called when the form is displayed.
     """
 
+    def update(self):
+        """
+        This method is called when the edit form initially loads,
+        before the user submits the form.
+        """
+        # Call the parent update() to proceed with normal form initialization
+        super(ProductEditForm, self).update()
+
+        # Perform custom actions when form loads
+        self.handle_form_load()
+
+    def updateFields(self):
+        super(ProductEditForm, self).updateFields()
+        # custom field tweaks here, AFTER calling super()
+
+    def updateWidgets(self):
+        super(ProductEditForm, self).updateWidgets()
+        # custom widget tweaks here, AFTER calling super()
+        # (self.groups is only populated once super() has run)
+
     def is_reviewer(self, context):
         # Check if person has reviewer role on context
         try:
             return checkPermission('cmf.ReviewPortalContent', self.context)
-    
+
         except NoInteraction:
             # If we're running this through a script, just assume
             # we can review.
@@ -58,47 +79,42 @@ class ProductEditForm(DefaultEditForm):
         # Traverse up until we hit product or a Plone site
         # Iterate up through the acquisition chain
         for o in aq_chain(self.context):
-    
+
             # Break out if we've made it up to the Plone site.
             if ISiteRoot.providedBy(o):
                 break
-    
+
             # If the item in the aq_chain is a product.
             # If our parent is a product defined as a product
             if IAtlasProduct.providedBy(o):
                 return(o)
 
-    def update(self):
-        """
-        This method is called when the edit form initially loads,
-        before the user submits the form.
-        """
-        # Perform custom actions when form loads
-        self.handle_form_load()
-        
-        # Call the parent update() to proceed with normal form initialization
-        super().update()
-    
     def handle_form_load(self):
+        request_url = self.request.getURL()
+
+        # Skip Widget URLs
+        if '++widget++' in request_url:
+            return
+
         product = self.product
         user_id = self.user_id
 
         if product and user_id:
             logger.info(f"Edit form loaded for: (Title: {product.Title()})")
-    
+
             # Example: Pre-populate form data, set defaults, log form access
             logger.info(f"User is editing: {product.absolute_url()}")
-            
+
             if IAtlasWebTeamReviewer.providedBy(product) and \
                self.is_reviewer(product) and \
                self.is_pending(product):
 
                 # Change the status
                 self.wftool.doActionFor(product, 'under_review', comment=f"{user_id} started editing.")
-    
+
                 # Set reviewer
                 setattr(product.aq_base, 'web_team_reviewer', [user_id,])
                 product.reindexObject()
 
-ProductEditView = layout.wrap_form(ProductEditForm)
-classImplements(ProductEditView, IDexterityEditForm)
+class ProductEditView(DefaultEditView):
+    form = ProductEditForm
